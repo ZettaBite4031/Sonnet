f42e7ce51df321ac199c49f7201e6c9a0ca0c320
ZettaBite4031 <ddayboy123@gmail.com>
2025-11-14 20:00:46 -0500

    Properly use the newest version of Catch2 since apt only ships v2 not v3

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,13 @@
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 
 #include "sonnet/sonnet.hpp"
 
 #include <random>
 #include <limits>
 #include <print>
+
+using namespace Catch;
 
 namespace {
   
